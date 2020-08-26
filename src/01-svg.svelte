c76c2fdd31356eeef4fefd0fa409f06ea9c416d4
Paul Butler <paulgb@gmail.com>
2020-08-26 06:56:37 -0400

    svg

---
+++ src/01-svg.svelte
@@ -0,0 +1,18 @@
+<script></script>
+
+<style>
+    svg {
+        border: 1px solid red;
+        width: 100%;
+        height: 500px;
+    }
+</style>
+
+<svg>
+    <g transform="translate(30, 30) scale(5)">
+        <rect width="90" height="30" fill="green" />
+        <circle r="10" fill="red" stroke="blue" stroke-width="8" />
+    </g>
+
+    <line stroke="black" x1="50" y1="80" stroke-width="8" />
+</svg>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import App from './index.svelte';
+// import App from './index.svelte';
+import App from './01-svg.svelte';
 
 const app = new App({
 	target: document.body,
